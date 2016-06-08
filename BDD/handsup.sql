5c712b2191409e25190e542dece768ad626aec01
Dimitri <dm@dm.com>
2016-06-08 10:57:19 +0200

    BDD

--- BDD/handsup.sql
+++ BDD/handsup.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.1
+-- version 4.4.15.5
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 07 Juin 2016 à 14:08
--- Version du serveur :  5.6.22
--- Version de PHP :  5.6.3
+-- Généré le :  Mer 08 Juin 2016 à 09:27
+-- Version du serveur :  5.6.30
+-- Version de PHP :  5.3.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `handsup`
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `cours` (
-`id` int(5) NOT NULL,
+  `id` int(5) NOT NULL,
   `libelle` varchar(50) NOT NULL,
   `description` varchar(500) NOT NULL,
   `nomFichier` varchar(20) NOT NULL
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `message` (
 --
 
 CREATE TABLE IF NOT EXISTS `question` (
-`id` int(10) NOT NULL,
+  `id` int(10) NOT NULL,
   `libelle` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -80,7 +80,18 @@ CREATE TABLE IF NOT EXISTS `reponse` (
   `id` int(10) NOT NULL,
   `libelle` varchar(50) NOT NULL,
   `bonne` tinyint(1) NOT NULL,
-  `nomImage` varchar(50) NOT NULL
+  `nomImage` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `suivre_cours`
+--
+
+CREATE TABLE IF NOT EXISTS `suivre_cours` (
+  `idUtilisateur` int(3) NOT NULL,
+  `idCours` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,11 +101,11 @@ CREATE TABLE IF NOT EXISTS `reponse` (
 --
 
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-`id` int(3) NOT NULL,
+  `id` int(3) NOT NULL,
   `login` varchar(20) NOT NULL,
   `motDePasse` varchar(20) NOT NULL,
   `statut` tinyint(1) NOT NULL,
-  `telephone` int(10) NOT NULL
+  `valide` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -105,19 +116,31 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 -- Index pour la table `cours`
 --
 ALTER TABLE `cours`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `question`
 --
 ALTER TABLE `question`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `reponse`
+--
+ALTER TABLE `reponse`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `suivre_cours`
+--
+ALTER TABLE `suivre_cours`
+  ADD PRIMARY KEY (`idUtilisateur`,`idCours`);
 
 --
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -127,17 +150,22 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour la table `cours`
 --
 ALTER TABLE `cours`
-MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `question`
 --
 ALTER TABLE `question`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `reponse`
+--
+ALTER TABLE `reponse`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
