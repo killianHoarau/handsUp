15400d75cb7375d7eb8c53d9c7c2a0369c69c2a8
Lilian <lilian78800@hotmail.fr>
2016-06-08 17:53:37 +0200

    BDD

--- BDD/handsup.sql
+++ BDD/handsup.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.5
+-- version 4.3.1
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 08 Juin 2016 à 10:44
--- Version du serveur :  5.6.30
--- Version de PHP :  5.3.29
+-- Généré le :  Mer 08 Juin 2016 à 17:52
+-- Version du serveur :  5.6.22
+-- Version de PHP :  5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `handsup`
@@ -23,14 +23,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `code_statut`
+--
+
+CREATE TABLE IF NOT EXISTS `code_statut` (
+  `code` varchar(5) NOT NULL,
+  `statut` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `code_statut`
+--
+
+INSERT INTO `code_statut` (`code`, `statut`) VALUES
+('00000', 0),
+('11111', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `cours`
 --
 
 CREATE TABLE IF NOT EXISTS `cours` (
-  `id` int(5) NOT NULL,
+`id` int(5) NOT NULL,
   `libelle` varchar(50) NOT NULL,
   `description` varchar(500) NOT NULL,
-  `nomFichier` varchar(20) DEFAULT NULL
+  `nomFichier` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -53,9 +72,8 @@ CREATE TABLE IF NOT EXISTS `message` (
 --
 
 CREATE TABLE IF NOT EXISTS `question` (
-  `id` int(10) NOT NULL,
-  `libelle` varchar(500) NOT NULL,
-  `verrouille` tinyint(1) NOT NULL DEFAULT '0'
+`id` int(10) NOT NULL,
+  `libelle` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -78,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `repondre` (
 --
 
 CREATE TABLE IF NOT EXISTS `reponse` (
-  `id` int(10) NOT NULL,
+`id` int(10) NOT NULL,
   `libelle` varchar(50) NOT NULL,
   `bonne` tinyint(1) NOT NULL,
   `nomImage` varchar(50) DEFAULT NULL
@@ -102,46 +120,64 @@ CREATE TABLE IF NOT EXISTS `suivre_cours` (
 --
 
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `id` int(3) NOT NULL,
+`id` int(3) NOT NULL,
   `login` varchar(20) NOT NULL,
   `motDePasse` varchar(20) NOT NULL,
   `statut` tinyint(1) NOT NULL,
-  `valide` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `valide` tinyint(1) NOT NULL DEFAULT '0',
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `login`, `motDePasse`, `statut`, `valide`, `email`) VALUES
+(1, 'jp', 'm', 1, 1, 'jp@m.fr'),
+(4, 'Elise', '1', 0, 0, 'elise@gmail.com'),
+(11, 'ElisePoirier', 'elise', 0, 0, 'elisepoirier@outlook.com'),
+(25, 'login', '1', 0, 1, 'lol@lolo.fr'),
+(28, 'lol', 'lol', 0, 1, 'lol@lol.fr');
 
 --
 -- Index pour les tables exportées
 --
 
 --
+-- Index pour la table `code_statut`
+--
+ALTER TABLE `code_statut`
+ ADD PRIMARY KEY (`code`);
+
+--
 -- Index pour la table `cours`
 --
 ALTER TABLE `cours`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `question`
 --
 ALTER TABLE `question`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `reponse`
 --
 ALTER TABLE `reponse`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `suivre_cours`
 --
 ALTER TABLE `suivre_cours`
-  ADD PRIMARY KEY (`idUtilisateur`,`idCours`);
+ ADD PRIMARY KEY (`idUtilisateur`,`idCours`);
 
 --
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -151,22 +187,22 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour la table `cours`
 --
 ALTER TABLE `cours`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `question`
 --
 ALTER TABLE `question`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `reponse`
 --
 ALTER TABLE `reponse`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
