a245525c9182bb89c1d40ba694bd6d093f56b347
killian HOARAU <killian HOARAU>
2016-06-10 09:11:03 +0200

    responsive inscription

--- Sources/pages/inscriptionConnexion.php
+++ Sources/pages/inscriptionConnexion.php
@@ -3,20 +3,18 @@
 	include("header.php");
 ?>
 
-<br>
-<div class="container">
-	<div class="col-lg-12">
-		<div class="col-lg-6">
-			<div class="col-lg-12">
-				<div class="titleprghp">
+<section id="feature" class="row">
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="titleprghp wow fadeInDown">
 					<span class="prg">Inscription</span> 
 				</div>
 			</div>
-			<div id="statutInscription" class='col-lg-12'>
-				<span id="erreurVide" class='popupW col-lg-12'>Veuillez remplir les champs</span>
-				</div>
-			<div id="signIn" class="msform col-lg-12">
-
+			<div id="statutInscription" class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+				<span id="erreurVide" class='popupW'>Veuillez remplir les champs en rouge</span>
+			</div>
+			<div id="signIn" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 wow fadeInDown msform">
 				<!-- fieldsets -->
 				<fieldset>
 					<input type="text" name="login" placeholder="Login"/>
@@ -29,17 +27,17 @@
 			</div>
 		</div>
 
-		<div class="col-lg-6">
-			<div class="col-lg-12">
-				<div class="titleprghp">
+		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="titleprghp wow fadeInDown">
 					<span class="prg">Connexion</span> 
 				</div>
 			</div>
-				<div id="statutConnexion" class='col-lg-12'>
-					<span id="connectVide" class='popupW col-lg-12'>Veuillez remplir les champs</span>
-				</div>
+			<div id="statutConnexion" class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+				<span id="connectVide" class='popupW'>Veuillez remplir les champs en rouge</span>
+			</div>
 			
-				<div id="Connect" class="msform col-lg-12">
+				<div id="Connect" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 wow fadeInDown msform">
 					<!-- fieldsets -->
 					<fieldset>
 						<input type="text" name="login" placeholder="Login" />
@@ -49,43 +47,17 @@
 					</fieldset>
 				</div>		
 				
-				<div id='formCompteOublie' class='msform formCompteOublie col-md-12'>
+				<div id='formCompteOublie' class='col-xs-12 col-sm-12 col-md-12 col-lg-12 msform formCompteOublie'>
 					<fieldset>
-								<span id="recupSpan" class='col-md-12' style='position: relative; top: 0px;'>Remplissez les données dont vous vous souvenez</span>
+								<span id="recupSpan" class='col-xs-12 col-sm-12 col-md-12 col-lg-12' style='position: relative; top: 0px;'>Remplissez les données dont vous vous souvenez</span>
 								<input name='login' placeholder="Login"/>
 								<input name='email' placeholder="email"/>
-								<input id='btnRecuperation' type="submit" class="next action-button recupererBnt" value="Recupérer mes identifiants" />
+								<input id='btnRecuperation' type="submit" class="next action-button recupererBnt" value="Envoyer" />
 					</fieldset>
 				</div>
 		</div>
 	</div>	  
-</div> 
-	<!-------------------------------------------------------------DEBUT DU POPUP ERREUR AVEC FORMULAIRE 
-		<div class="formConnexion msform">
-		<form action="../php/confirmOubli.php" method="POST" >
-			<!-- fieldsets 
-			<fieldset>
-				<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<div class="titleprghp">
-					<h2 id="myModalLabel">Récupération</h2>
-				</div>
-				<h3>
-					<br />Vous avez oublié votre mot de passe ou votre login?
-					<br />Aucun souci, remplisser les informations que vous connaissez.
-				</h3>
-				<input type="text" name="login" placeholder="Login" />
-				<input type="email" name="email" placeholder="Email"/>
-				<div class="col-lg-6"><input type="submit" name="next" class="next action-button" value="Envoie" /></div>
-			</fieldset>
-		</form>
-	</div>-->
-</div>	 
-	<?php
-		// include("popUp_ConfirmInscription.php");
-		// include("popUp_Oubli.php");
-		// include("popUp_Erreur.php");
-	?>
-</div>
+</section> 
 
 <?php
 	$nomScript="affiche_popUp_ConfirmationInscription";
