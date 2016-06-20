525d589a66f07f6df087d0bd583cd0130d60c99c
Lilian <jpmaligne@gmail.com>
2016-06-20 16:28:43 +0200

    Messagerie css

--- Sources/pages/messagerie.php
+++ Sources/pages/messagerie.php
@@ -3,7 +3,7 @@
     include("header.php");
 	$login = $_SESSION['login'];
 	$id = $_SESSION['id'];
-	$query = "SELECT message.id, message.contenu, message.date, message.reponse, message.lu, u1.login as 'emetteur', u2.login as 'destinataire' FROM message, utilisateur u1, utilisateur u2 WHERE message.idEmetteur = u1.id AND message.idDestinataire = u2.id AND idEmetteur = $id OR idDestinataire = $id;";
+	$query = "SELECT distinct id FROM conversation WHERE utilisateur0 = $id or utilisateur1 = $id";
 	//echo $query;
 	$result = $link->query($query);
 ?>
@@ -12,29 +12,31 @@
 		<div class="center wow fadeInDown animated">
 			<h2>Vos messages</h2>
 <?php
-			while($row=$result->fetch_assoc())
-			{  if($row['lu']==0)
-				{?>	<!-- Changer la classe de la div si le message n'est pas lu (genre txt en gras) -->
-					<div id=<?php echo $row['id']; ?> class="nonLu" name="Message">
-<?php 			} else{?>
-					<div id=<?php echo $row['id']; ?> class="Lu" name="Message">
-<?php			}?>	
-					<span><?php echo $row['emetteur']; ?></span>
-					<span><?php echo $row['titre']; ?></span>
-					<span><?php echo $row['date']; ?></span>
-					<!-- Champs CACHES -->
-					<input type="hidden" value="<?php echo $row['emetteur']; ?>" name="<?php echo $row['id']; ?>emetteur" />
-					<input type="hidden" value="<?php echo $row['titre']; ?>" name="<?php echo $row['id']; ?>titre" />
-					<input type="hidden" value="<?php echo $row['destinataire']; ?>" name="<?php echo $row['id']; ?>destinataire" />
+			while($row=$result->fetch_assoc())	//Pour chaque Conversation
+			{  
+				$idConversation = $row['id'];
+				$query = "SELECT * FROM message_prive WHERE idConversation = $idConversation ORDER BY date DESC;";
+				$res = $link->query($query);
+				while($message = $res->fetch_assoc())
+				{
+					if($message['lu']==0)
+					{?>	<!-- Changer la classe de la div si le message n'est pas lu (genre txt en gras) -->
+						<div id=<?php echo $message['id']; ?> class="nonLu" name="Message">
+	<?php 			} else{?>
+						<div id=<?php echo $message['id']; ?> class="Lu" name="Message">
+	<?php			}?>	
+						<span><?php echo $row['idEmetteur']; ?></span>
+						<span><?php echo $message['titre']; ?></span>
+						<span><?php echo $message['date']; ?></span>
+						
+					</div>
 					
-				</div>
-				
-				<!--Div deroulé, affiche le message et l'editeur de texte pour repondre-->
-				<div id="reponse<?php echo $row['id']; ?>">
-					<span name="<?php echo $row['id']; ?>contenu" /><?php echo $row['contenu']; ?></span>
-					<span><?php echo $row['date']; ?><span>
-				</div>
-<?php		} ?>
+					<!--Div deroulé, affiche le message et l'editeur de texte pour repondre-->
+					<div id="reponse<?php echo $row['id']; ?>">
+						<span name="<?php echo $row['id']; ?>contenu" /><?php echo $message['contenu']; ?></span>
+						<span><?php echo $row['date']; ?><span>
+					</div>
+			<?php		} }?>
 		</div>
 	</div>
 	
