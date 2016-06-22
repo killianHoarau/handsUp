33b8edabd529ebf1b84e2f27b76aa97b3305ea3d
Lilian <jpmaligne@gmail.com>
2016-06-22 12:02:08 +0200

    Ajout d'un message

--- Sources/ajax/ajoutMessage.php
+++ Sources/ajax/ajoutMessage.php
@@ -5,22 +5,36 @@
 	$titre = $_POST['titre'];
 	$contenu = $_POST['contenu'];
 	$tabDestinataire = $_POST['tabDestinataire'];
+	$date = date("Y-m-d");
 	
 	for($i=0; $i<count($tabDestinataire); $i++)
 	{
 		$idDestinataire = $tabDestinataire[$i];
-		$query = "SELECT id FROM conversation WHERE (utilisateur0 = $id AND utilisateur1 = $idDestinataire) or (utilisateur0 = $idDestinataire AND utilisateur1 = $id);"
+		$query = "SELECT id FROM conversation WHERE (utilisateur0 = $id AND utilisateur1 = $idDestinataire) or (utilisateur0 = $idDestinataire AND utilisateur1 = $id);";
 		$result = $link->query($query);
-		if(mysql_num_rows($result) > 0)	//Si la conversation exist deja en base on recupere son id et on INSERT un message qui possède cet id Conversation
+		$row = $result->fetch_assoc();
+		if($result->num_rows > 0)	//Si la conversation exist deja en base on recupere son id et on INSERT un message qui possède cet id Conversation + UPDATE de la date de la conversation
 		{
-			
+			$idConversation = $row['id'];
+			$insertMessage = "INSERT INTO message_prive values ('', $id, $idDestinataire, '$titre', '$contenu', '$date', 0, $idConversation);";
+			$resInsert = $link->query($insertMessage);
+			$updateConversation = "UPDATE conversation set date = '$date' WHERE id = $idConversation;";
+			$link->query($updateConversation);
 		}
 		else	//Sinon la conversation est créée et INSERT du message sur ce nouvel idConversation.
 		{
+			$insertConversation = "INSERT INTO conversation values ('', $id, $idDestinataire, '$date');";
+			$link->query($insertConversation);
 			
+			$selectMaxConversation = "SELECT MAX(id) FROM conversation;";
+			$result = $link->query($selectMaxConversation);
+			$ligne = $result->fetch_assoc();
+			$idConversation = $ligne['MAX(id)'];
+			$insertMessage = "INSERT INTO message_prive values ('', $id, $idDestinataire, '$titre', '$contenu', '$date', 0, $idConversation);";
+			// echo $insertMessage;
+			$link->query($insertMessage);
 		}
 	}
-	
 	
 	
 	
